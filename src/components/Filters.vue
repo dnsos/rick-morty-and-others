51db0022b5e76a31453ce1141b89e54e29af2abb
Dennis Ostendorf <dennisostendorf@web.de>
2020-02-21 13:55:19 +0100

    Scaffold Filters component

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,14 +1,60 @@
 <template>
   <section class="filters">
     <h2>Filters</h2>
-    <p>Filter status</p>
-    <p>Filter gender</p>
+    <div class="status-filter">
+      <fieldset>
+        <legend>Show status</legend>
+        <div v-for="(status, index) in status" :key="index">
+          <input
+            type="checkbox"
+            :id="`status-${status}`"
+            :value="status"
+            v-model="selectedStatus"
+          >
+          <label :for="`status-${status}`">{{ status }}</label>
+        </div>
+      </fieldset>
+    </div>
+    <div class="gender-filter">
+      <fieldset>
+        <legend>Show genders</legend>
+        <div v-for="(gender, index) in gender" :key="index">
+          <input
+            type="checkbox"
+            :id="`gender-${gender}`"
+            :value="gender"
+            v-model="selectedGender"
+          >
+          <label :for="`gender-${gender}`">{{ gender }}</label>
+        </div>
+      </fieldset>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'Filters'
+  name: 'Filters',
+  props: {
+    status: {
+      type: Array,
+      default: function () {
+        return ['Alive', 'Dead', 'unknown']
+      }
+    },
+    gender: {
+      type: Array,
+      default: function () {
+        return ['Female', 'Male', 'Genderless', 'unknown']
+      }
+    }
+  },
+  data() {
+    return {
+      selectedStatus: this.status,
+      selectedGender: this.gender
+    }
+  }
 }
 </script>
 
